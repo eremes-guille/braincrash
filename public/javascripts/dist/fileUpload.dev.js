9bfcb51cf345ce3cb89816992849e8404b92b428
arifin <72711522+eremes-guille@users.noreply.github.com>
2020-11-01 21:26:19 +0700

    finished copy

--- public/javascripts/dist/fileUpload.dev.js
+++ public/javascripts/dist/fileUpload.dev.js
@@ -1,9 +1,22 @@
 "use strict";
 
-FilePond.registerPlugin(FilePondPluginImagePreview, FilePondPluginImageResize, FilePondPluginFileEncode);
-FilePond.setOptions({
-  stylePanelAspectRatio: 150 / 100,
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150
-});
-FilePond.parse(document.body);
+var rootStyles = window.getComputedStyle(document.documentElement);
+
+if (rootStyles.getPropertyValue('book-cover-width-large') != null) {
+  ready();
+} else {
+  document.getElementById('main-css').addEventListener('load', ready);
+}
+
+function ready() {
+  var coverWidth = parseFloat(rootStyles.getPropertyValue('book-cover-width-large'));
+  var coverAspectRatio = parseFloat(rootStyles.getPropertyValue('book-cover-width-ratio'));
+  var coverHeight = coverWidth / coverAspectRatio;
+  FilePond.registerPlugin(FilePondPluginImagePreview, FilePondPluginImageResize, FilePondPluginFileEncode);
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight
+  });
+  FilePond.parse(document.body);
+}
